0562b76b5db04d9980e0a8cb7f4734140ba60138
Jakirhuseain <jakirhuseain530@gmail.com>
2025-07-19 18:51:12 +0530

    Aging Report changes

--- modules/purchase/views/billing_reports/table_aging_report.php
+++ modules/purchase/views/billing_reports/table_aging_report.php
@@ -1,31 +1,45 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-$select = [
-    'vendor_name',
-    'invoice_no',
-    'invoice_date',
-    'invoice_amount',
-    'days_since_invoice',
-    'billing_status',
-];
-$where = [];
 $this->ci->load->model('purchase/purchase_model');
+$base_currency = get_base_currency_pur();
+
+$select = [
+    'pv.company as vendor_name',
+    'pi.vendor_invoice_number as invoice_no',
+    'pi.invoice_date as invoice_date',
+    'pi.final_certified_amount as invoice_amount',
+    'DATEDIFF(CURRENT_DATE, pi.invoice_date) as days_since_invoice',
+    'pi.payment_status as billing_status'
+];
+
+$join = [
+    'LEFT JOIN ' . db_prefix() . 'pur_vendor pv ON pv.userid = pi.vendor',
+    'LEFT JOIN ' . db_prefix() . 'itemable it ON it.vbt_id = pi.id AND it.rel_type = "invoice"',
+    'LEFT JOIN ' . db_prefix() . 'invoices inv ON inv.id = it.rel_id',
+];
+
+$where = [];
 $custom_date_select = $this->ci->purchase_model->get_where_report_period('invoice_date');
 if ($custom_date_select != '') {
-    array_push($where, $custom_date_select);
+    $custom_date_select = trim($custom_date_select);
+    if (!startsWith($custom_date_select, 'AND')) {
+        $custom_date_select = 'AND ' . $custom_date_select;
+    }
+    $where[] = $custom_date_select;
 }
+$where[] = 'AND (inv.id IS NULL OR pi.payment_status IN (0, 2, 3, 4))';
 
-$aColumns     = $select;
-$sIndexColumn = 'id';
+$additionalSelect = [
+    'pv.userid as vendor_id'
+];
 
-$result = data_tables_init_for_billing_aging_reports($aColumns, $sIndexColumn, '', [], $where, [
-	'vendor_id',
-]);
+$sIndexColumn = 'pi.id';
+$sTable       = db_prefix() . 'pur_invoices pi';
 
+$result  = data_tables_init($select, $sIndexColumn, $sTable, $join, $where, $additionalSelect);
 $output  = $result['output'];
 $rResult = $result['rResult'];
-$base_currency = get_base_currency_pur();
 
 foreach ($rResult as $aRow) {
     $row = [];
@@ -35,18 +49,24 @@ foreach ($rResult as $aRow) {
     $row[] = date('d-M-Y', strtotime($aRow['invoice_date']));
     $row[] = app_format_money($aRow['invoice_amount'], $base_currency->symbol);
     $row[] = $aRow['days_since_invoice'];
-    $payment_status = '';
-    if($aRow['payment_status'] == 0) {
-        $payment_status = _l('unpaid');
-    } else if ($aRow['payment_status'] == 2) {
-        $payment_status = _l('recevied_with_comments');
-    } else if ($aRow['payment_status'] == 3) {
-        $payment_status = _l('bill_verification_in_process');
-    } else if ($aRow['payment_status'] == 4) {
-        $payment_status = _l('bill_verification_on_hold');
-    } else {
-        $payment_status = _l('Pending');
+
+    switch ($aRow['billing_status']) {
+        case 0:
+            $payment_status = _l('unpaid');
+            break;
+        case 2:
+            $payment_status = _l('recevied_with_comments');
+            break;
+        case 3:
+            $payment_status = _l('bill_verification_in_process');
+            break;
+        case 4:
+            $payment_status = _l('bill_verification_on_hold');
+            break;
+        default:
+            $payment_status = _l('Pending');
     }
+
     $row[] = $payment_status;
 
     $output['aaData'][] = $row;
