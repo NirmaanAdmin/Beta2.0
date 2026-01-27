66caf94bb171be5cf7a53784f411b98d45713592
Pawan Kumar <happyk682@gmail.com>
2026-01-27 11:43:32 +0530

    some changes

--- modules/purchase/views/assar/assar.php
+++ modules/purchase/views/assar/assar.php
@@ -48,63 +48,83 @@ $module_name = 'module_activity_log'; ?>
                            </a>
                         </div>
                      </div>
-
-
                      <br>
-
-
-                     <div class="btn-group show_hide_columns" id="show_hide_columns">
-                        <!-- Settings Icon -->
-                        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="padding: 4px 7px;">
-                           <i class="fa fa-cog"></i> <?php  ?> <span class="caret"></span>
-                        </button>
-                        <!-- Dropdown Menu with Checkboxes -->
-                        <div class="dropdown-menu" style="padding: 10px; min-width: 250px;">
-                           <!-- Select All / Deselect All -->
-                           <div>
-                              <input type="checkbox" id="select-all-columns"> <strong><?php echo _l('select_all'); ?></strong>
+                     <div class="row">
+                        <div class="col-md-12">
+                           <div class="horizontal-tabs">
+                              <ul class="nav nav-tabs nav-tabs-horizontal mbot15" role="tablist">
+                                 <li role="presentation" class="active">
+                                    <a href="#master" aria-controls="master" role="tab" id="tab_master" data-toggle="tab">
+                                       Master
+                                    </a>
+                                 </li>
+                                 <li role="presentation">
+                                    <a href="#main_sheet" aria-controls="main_sheet" role="tab" id="tab_main_sheet" data-toggle="tab">
+                                       Main Sheet
+                                    </a>
+                                 </li>
+                              </ul>
                            </div>
-                           <hr>
-                           <!-- Column Checkboxes -->
-                           <?php
-                           $columns = [
-                              'Client ID',
-                              'Name',
-                              'Phone',
-                              'Start Date',
-                              'Investment Amount',
-                              'Status (Active/Inactive)',
-                              'Referred by',
-                              'Remarks',
-                           ];
-                           ?>
-                           <div>
-                              <?php foreach ($columns as $key => $label): ?>
-                                 <input type="checkbox" class="toggle-column" data-id="<?php echo $label; ?>" value="<?php echo $key; ?>" checked>
-                                 <?php echo _l($label); ?><br>
-                              <?php endforeach; ?>
-                           </div>
+                        </div>
+                        <div class="tab-content">
+                           <div role="tabpanel" class="col-md-12 tab-pane tracker-pane active" id="master">
+                              <div class="btn-group show_hide_columns" id="show_hide_columns">
+                                 <!-- Settings Icon -->
+                                 <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="padding: 4px 7px;">
+                                    <i class="fa fa-cog"></i> <?php  ?> <span class="caret"></span>
+                                 </button>
+                                 <!-- Dropdown Menu with Checkboxes -->
+                                 <div class="dropdown-menu" style="padding: 10px; min-width: 250px;">
+                                    <!-- Select All / Deselect All -->
+                                    <div>
+                                       <input type="checkbox" id="select-all-columns"> <strong><?php echo _l('select_all'); ?></strong>
+                                    </div>
+                                    <hr>
+                                    <!-- Column Checkboxes -->
+                                    <?php
+                                    $columns = [
+                                       'Client ID',
+                                       'Name',
+                                       'Phone',
+                                       'Start Date',
+                                       'Investment Amount',
+                                       'Status (Active/Inactive)',
+                                       'Referred by',
+                                       'Remarks',
+                                    ];
+                                    ?>
+                                    <div>
+                                       <?php foreach ($columns as $key => $label): ?>
+                                          <input type="checkbox" class="toggle-column" data-id="<?php echo $label; ?>" value="<?php echo $key; ?>" checked>
+                                          <?php echo _l($label); ?><br>
+                                       <?php endforeach; ?>
+                                    </div>
 
+                                 </div>
+                              </div>
+
+                              <table class="dt-table-loading table table-table_assar">
+                                 <thead>
+                                    <tr>
+                                       <th><?php echo _l('Client ID'); ?></th>
+                                       <th><?php echo _l('Name'); ?></th>
+                                       <th><?php echo _l('Phone'); ?></th>
+                                       <th><?php echo _l('Start Date'); ?></th>
+                                       <th><?php echo _l('Investment Amount'); ?></th>
+                                       <th><?php echo _l('Status (Active/Inactive)'); ?></th>
+                                       <th><?php echo _l('Referred by'); ?></th>
+                                       <th><?php echo _l('Remarks'); ?></th>
+                                    </tr>
+                                 </thead>
+                                 <tbody></tbody>
+                                 <tbody></tbody>
+                              </table>
+                           </div>
+                           <div role="tabpanel" class="col-md-12 tab-pane tracker-pane active" id="main_sheet">
+                              Main Sheet Content Goes Here
+                           </div>
                         </div>
                      </div>
-
-                     <table class="dt-table-loading table table-table_assar">
-                        <thead>
-                           <tr>
-                              <th><?php echo _l('Client ID'); ?></th>
-                              <th><?php echo _l('Name'); ?></th>
-                              <th><?php echo _l('Phone'); ?></th>
-                              <th><?php echo _l('Start Date'); ?></th>
-                              <th><?php echo _l('Investment Amount'); ?></th>
-                              <th><?php echo _l('Status (Active/Inactive)'); ?></th>
-                              <th><?php echo _l('Referred by'); ?></th>
-                              <th><?php echo _l('Remarks'); ?></th>
-                           </tr>
-                        </thead>
-                        <tbody></tbody>
-                        <tbody></tbody>
-                     </table>
-
                   </div>
                </div>
             </div>
