1ce39510e0b9d9660020246d9d65d4cdeab9905e
Jakirhuseain <jakirhuseain530@gmail.com>
2026-02-24 18:18:27 +0530

    Cashflow charts changes

--- modules/purchase/views/cashflow/manage.php
+++ modules/purchase/views/cashflow/manage.php
@@ -21,6 +21,17 @@
          <div class="panel_s mbot10">
             <div class="panel-body">
                <div class="row">
+                  <div class="col-md-12">
+                     <div style="width: 100%; height: 550px;">
+                       <canvas id="cashflowChart"></canvas>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="panel_s mbot10">
+            <div class="panel-body">
+               <div class="row">
                   <table class="table dt-table border">
                      <thead>
                         <th><?php echo _l('Timeline'); ?></th>
@@ -45,6 +56,7 @@
 
 <?php init_tail(); ?>
 <script>
+   var cashflowChart = null;
    $(document).ready(function() {
       "use strict";
       load_cashflow_data();
@@ -54,32 +66,143 @@
    });
 
    function load_cashflow_data() {
-     var total_months = $('input[name="total_months"]').val();
-     var start_date   = $('input[name="start_date"]').val();
-     var budgeted     = $('input[name="budgeted"]').val();
-     $.post(admin_url + 'purchase/get_cashflow_data', {
-      total_months: total_months,
-      start_date: start_date,
-      budgeted: budgeted
-     }, function(response){
-         var data = JSON.parse(response);
-         var tbody = '';
-         $.each(data, function(i, row){
-            tbody += '<tr>';
-            tbody += '<td>'+row.timeline+'%</td>';
-            tbody += '<td>'+row.cumulative_cashflow+'%</td>';
-            tbody += '<td>'+row.months_cal+'</td>';
-            tbody += '<td>'+row.actual_forecast_percentage.toFixed(2)+'%</td>';
-            tbody += '<td>'+row.months_cal_name+'</td>';
-            tbody += '<td>'+format_money(row.monthly_cashflow_value)+'</td>';
-            tbody += '<td>'+format_money(row.cumulative_cashflow_value)+'</td>';
-            tbody += '<td>'+format_money(row.forecast_monthly_cashflow)+'</td>';
-            tbody += '<td>'+format_money(row.actual_cumulative_cashflow)+'</td>';
-            tbody += '</tr>';
-         });
-         $('table tbody').html(tbody);
-     });
+      var total_months = $('input[name="total_months"]').val();
+      var start_date   = $('input[name="start_date"]').val();
+      var budgeted     = $('input[name="budgeted"]').val();
+      $.post(admin_url + 'purchase/get_cashflow_data', {
+         total_months: total_months,
+         start_date: start_date,
+         budgeted: budgeted
+        }, function(response){
+            var data = JSON.parse(response);
+            var tbody = '';
+            var months_cal_name = [];
+            var monthly_cashflow_value = [];
+            var forecast_monthly_cashflow = [];
+            var cumulative_cashflow_value = [];
+            var actual_cumulative_cashflow = [];
+            if (Array.isArray(data) && data.length > 0) {
+               $.each(data, function(i, row){
+                  months_cal_name.push(row.months_cal_name);
+                  monthly_cashflow_value.push(parseFloat(row.monthly_cashflow_value) || 0);
+                  forecast_monthly_cashflow.push(parseFloat(row.forecast_monthly_cashflow) || 0);
+                  cumulative_cashflow_value.push(parseFloat(row.cumulative_cashflow_value) || 0);
+                  actual_cumulative_cashflow.push(parseFloat(row.actual_cumulative_cashflow) || 0);
+                  tbody += '<tr>';
+                  tbody += '<td>'+row.timeline+'%</td>';
+                  tbody += '<td>'+row.cumulative_cashflow+'%</td>';
+                  tbody += '<td>'+row.months_cal+'</td>';
+                  tbody += '<td>'+parseFloat(row.actual_forecast_percentage).toFixed(2)+'%</td>';
+                  tbody += '<td>'+row.months_cal_name+'</td>';
+                  tbody += '<td>'+format_money(row.monthly_cashflow_value)+'</td>';
+                  tbody += '<td>'+format_money(row.cumulative_cashflow_value)+'</td>';
+                  tbody += '<td>'+format_money(row.forecast_monthly_cashflow)+'</td>';
+                  tbody += '<td>'+format_money(row.actual_cumulative_cashflow)+'</td>';
+                  tbody += '</tr>';
+               });
+
+            }
+            $('table tbody').html(tbody);
+            render_cashflow_chart(
+               months_cal_name,
+               monthly_cashflow_value,
+               forecast_monthly_cashflow,
+               cumulative_cashflow_value,
+               actual_cumulative_cashflow
+            );
+      });
+   }
+
+   function render_cashflow_chart(
+      months_cal_name,
+      monthly_cashflow_value,
+      forecast_monthly_cashflow,
+      cumulative_cashflow_value,
+      actual_cumulative_cashflow
+   ) {
+      var ctx = document.getElementById('cashflowChart').getContext('2d');
+      if (cashflowChart !== null) {
+         cashflowChart.destroy();
+      }
+      cashflowChart = new Chart(ctx, {
+         data: {
+            labels: months_cal_name,
+            datasets: [
+               {
+                  type: 'bar',
+                  label: 'Monthly Cashflow',
+                  data: monthly_cashflow_value,
+                  backgroundColor: '#1E90FF'
+               },
+               {
+                  type: 'bar',
+                  label: 'Forecast Monthly Cashflow',
+                  data: forecast_monthly_cashflow,
+                  backgroundColor: '#00A300'
+               },
+               {
+                  type: 'line',
+                  label: 'Cumulative Cashflow',
+                  data: cumulative_cashflow_value,
+                  borderColor: '#dc3545',
+                  backgroundColor: '#dc3545',
+                  tension: 0.3,
+                  fill: false
+               },
+               {
+                  type: 'line',
+                  label: 'Actual Cumulative Cashflow',
+                  data: actual_cumulative_cashflow,
+                  borderColor: '#A300A3',
+                  backgroundColor: '#A300A3',
+                  tension: 0.3,
+                  fill: false
+               }
+            ]
+         },
+         options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            interaction: {
+               mode: 'index',
+               intersect: false
+            },
+            plugins: {
+               legend: {
+                  position: 'bottom'
+               },
+               tooltip: {
+                  callbacks: {
+                     label: function(context) {
+                        return context.dataset.label + ': ' + format_money(context.raw);
+                     }
+                  }
+               }
+            },
+            scales: {
+               x: {
+                  title: {
+                     display: true,
+                     text: 'Month'
+                  }
+               },
+               y: {
+                  beginAtZero: true,
+                  title: {
+                     display: true,
+                     text: 'Value'
+                  },
+                  ticks: {
+                     callback: function(value) {
+                        return format_money(value);
+                     }
+                  }
+               }
+            }
+         }
+      });
    }
 </script>
+<script src="<?php echo module_dir_url(PURCHASE_MODULE_NAME, 'assets/plugins/charts/chart.js'); ?>?v=<?php echo PURCHASE_REVISION; ?>"></script>
 </body>
 </html>
